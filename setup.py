2010ba1bff8ebbd194e8bdba68e8e41dd3505746
Jeffrey Bean <jeffreyrobertbean@gmail.com>
2013-08-15 20:50:42 -0400

    using setuptools and including package data

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='jeffbean',
     version='1.0',
-    packages=['jbean', 'jbean.apps.blog'],
+    packages=find_packages(),
     url='lollyandbean.com',
     license='',
     author='Jeffrey Bean',
-    author_email='jeffreyrobertbean@gmail.com',
+    author_email='jrb3330@gmail.com',
     description="Laura and Jeff's Website",
     requires=['django'],
+    include_package_data=True,
 )
